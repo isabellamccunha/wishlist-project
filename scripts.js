41bafbcff20af1ee9a8bfa5409d7911cde252ca1
Isabella Cunha <isabellamdnt@gmail.com>
2024-06-04 19:15:20 -0300

    add: images

--- scripts.js
+++ scripts.js
@@ -1,29 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let slideIndex = 0;
-    const slides = document.querySelectorAll('.carousel .slides img');
-    const loginModal = document.getElementById('login-modal');
+document.addEventListener('DOMContentLoaded', function() {
     const loginLink = document.getElementById('login-link');
-    const closeButton = document.querySelector('.close-button');
+    const loginModal = document.getElementById('login-modal');
+    const closeModal = document.querySelector('.close-button');
 
-    const showSlides = () => {
-        slides.forEach((slide, index) => {
-            slide.style.display = index === slideIndex ? 'block' : 'none';
-        });
-        slideIndex = (slideIndex + 1) % slides.length;
-    };
-
-    setInterval(showSlides, 3000);
-
-    loginLink.addEventListener('click', () => {
+    loginLink.addEventListener('click', function(e) {
+        e.preventDefault();
         loginModal.style.display = 'flex';
     });
 
-    closeButton.addEventListener('click', () => {
+    closeModal.addEventListener('click', function() {
         loginModal.style.display = 'none';
     });
 
-    window.addEventListener('click', (event) => {
-        if (event.target === loginModal) {
+    window.addEventListener('click', function(e) {
+        if (e.target === loginModal) {
             loginModal.style.display = 'none';
         }
     });
